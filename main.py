53f626e75ca03138dfb1afdcadf1da16dd01f2b4
Kevin Luo <kvn.luo@gmail.com>
2023-07-27 16:56:51 -0400

    create post route for creating new recipes and added RecipeCreate model

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 from typing import Optional
-from fastapi import FastAPI, APIRouter
-from models import User
+from fastapi import FastAPI, APIRouter, Query
+from models import Recipe, RecipeSearchResults, RecipeCreate
 from recipes_data import RECIPES
 
-app = FastAPI(title="Recipe API", openapi_url="/openapi.json")  # app is an "instance" of the class FastAPI
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
 
+# Root Get
 @api_router.get("/", status_code=200)
 async def root() -> dict:
-    """
-    Root Get
-    """
     return {"message": "Who's there?"}
 
 
-@api_router.get("/recipe/{recipe_id}", status_code=200)
+# Fetch a single recipe by ID
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
 async def fetch_recipe(*, recipe_id: int) -> dict:
-    """
-    Fetch a single recipe by ID
-    """
     result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
     if result:
         return result[0]
 
 
-@api_router.get("/search/", status_code=200)
-async def search_recipes(keyword: Optional[str] = None, max_results: Optional[int] = 10) -> dict:
-    """
-    Search for recipes based on label keyword
-    """
+# Search for recipes based on label keyword
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+async def search_recipes(
+    *, keyword: Optional[str] = Query(None, min_length=3, example="chicken"), max_results: Optional[int] = 10
+) -> dict:
     if not keyword:
         return {"results": RECIPES[:max_results]}
 
     results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
     return {"results": list(results)[:max_results]}
+
+
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+async def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+
+    RECIPES.append(recipe_entry.dict())
+    return recipe_entry
 
 
 app.include_router(api_router)
--- models.py
+++ models.py
@@ -1,26 +1,46 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-from uuid import UUID, uuid4
-from enum import Enum
+from pydantic import BaseModel, Field, HttpUrl
+from typing import Sequence
+
+# from datetime import datetime
+# from uuid import UUID, uuid4
+# from enum import Enum
 
 
-class Gender(str, Enum):
-    male = "male"
-    female = "female"
+class Recipe(BaseModel):
+    id: int
+    label: str
+    source: str
+    url: HttpUrl
 
 
-class Role(str, Enum):
-    admin = "admin"
-    user = "user"
-    base = "base"
+class RecipeSearchResults(BaseModel):
+    results: Sequence[Recipe]
 
 
-class User(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
-    created: datetime = Field(default_factory=datetime.utcnow)
-    first_name: str
-    last_name: str
-    age: int
-    fav_color: str | None
-    gender: Gender
-    roles: list[Role]
+class RecipeCreate(BaseModel):
+    label: str
+    source: str
+    url: HttpUrl
+    submitter_id: int
+
+
+# class Gender(str, Enum):
+#     male = "male"
+#     female = "female"
+
+
+# class Role(str, Enum):
+#     admin = "admin"
+#     user = "user"
+#     base = "base"
+
+
+# class User(BaseModel):
+#     id: UUID = Field(default_factory=uuid4)
+#     created: datetime = Field(default_factory=datetime.utcnow)
+#     first_name: str
+#     last_name: str
+#     age: int
+#     fav_color: str | None
+#     gender: Gender
+#     roles: list[Role]
